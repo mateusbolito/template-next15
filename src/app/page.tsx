28338bf462b28ae89905dde5acae5ae48bf53424
Mateus Bolito <mateusbolito@hotmail.com>
2025-03-12 21:25:07 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const data = await res.json();
   console.log(data);
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center justify-center text-2xl ">
+    <div className="bg-gray-700 min-h-screen flex items-center justify-center text-2xl ">
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
